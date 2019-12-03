ac76681318da5dc2e19967fa8679697a5f8e6806
mk fuentes <miguelangel.fm@outlook.com>
2019-12-02 22:05:46 -0600

    cambio divisa

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("Hello Koders!")
+
+var valor_pesos = 100
+var valor_usd = 19
+var valor_eur = 22
+var valor_cuc = 5.5
+
+console.log(valor_pesos/valor_usd)
+console.log(valor_pesos/valor_eur)
+console.log(valor_pesos/valor_cuc*valor_usd)
+
+
+
