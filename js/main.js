f0e6020fa0ebfe89d460947ea1a4b18e6a03215b
mk fuentes <miguelangel.fm@outlook.com>
2019-12-03 19:08:23 -0600

    Tarea segundos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 console.log("Hello Koders!")
 
-var valor_pesos = 100
+
+/*Ejercicio1= */
+/*var valor_pesos = 100
 var valor_usd = 19
 var valor_eur = 22
 var valor_cuc = 5.5
 
 console.log(valor_pesos/valor_usd)
 console.log(valor_pesos/valor_eur)
-console.log(valor_pesos/valor_cuc*valor_usd)
+console.log(valor_pesos/valor_cuc*valor_usd)*/
 
+/*Ejercicio 2*/
+/*Cuantos segundos*/
 
+var dias_anio = 365
+var dias_mes = 30
+var seg_dia = 86400
+
+var edad_anio =27
+var edad_mes = 2
+var edad_dia = 10
+
+console.log((dias_anio*edad_anio*seg_dia+dias_mes*edad_mes*seg_dia+seg_dia*edad_dia)+" segundos")
 
