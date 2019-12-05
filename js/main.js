d7c52ecc8cfff8ab310dfbdc4a593f2c2e1bb5aa
mk fuentes <miguelangel.fm@outlook.com>
2019-12-04 22:04:44 -0600

    do, do while y for

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("Hello Koders!")
+
+var numbers = [1,2,3,4,5] 
+var ciclo = 0
+    while (ciclo < numbers.length){
+        console.log(numbers[ciclo])
+        ciclo++
+    }
+
+
+
+/*Verificar que la palabra tiene al menos 3 vocales*/
+var word = "League"
+var ciclos = 0
+    for (ciclo = 0; ciclo< word.length; ciclo++){
+        if (word[ciclo] === "a" || 
+        word[ciclo] === "e" || 
+        word[ciclo] === "i" || 
+        word[ciclo] === "o" ||  
+        word[ciclo] === "u") {
+        ciclos++
+        }
+    }
+            if(ciclos >=3) { 
+                console.log(ciclos + " vocales")
+    
+            }
+                    else {
+                        console.log("Al menos deben ser 3 caracteres")
+                    }
