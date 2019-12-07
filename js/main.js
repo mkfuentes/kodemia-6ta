8b2412112967f478f5cb29d83aa3a760ba4c6f85
mk fuentes <miguelangel.fm@outlook.com>
2019-12-06 18:47:29 -0600

    4th class functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-console.log("Hello Koders!")
+/*
+for (var index = 0; index <= 100; index++) {
+    console.log(`${index}`)    
+        if (index%2===0) {
+            console.log(`${index}    es numero par`)
+        }
+    
+}
+
+
+3.   Se tiene una lista con el nombre de cada uno de los empleados, con los días que faltaron al trabajo
+
+Ej -> [ [David, 4], [Jose, 2] ]
+
+Si el sueldo diario son $450.00, y por cada día que faltaron se le descuenta el 2% del salario (no acumulable), cuanto es el sueldo final del empleado.
+
+OUTPUT: {name}: ${salary}.00*/
+/*
+var empleado = ["David",4]
+var empleado2 = ["Jose",2]
+var sueldo = 13500
+var i = 0
+
+while (i<empleado[1]){
+var sueldoTotal = sueldo - (sueldo*.02)
+i++
+}
+
+
+console.log(`${empleado[0]} con sueldo ${sueldoTotal} `)
+
+*/
+
+var data = ["p","e","r","r","o"]
+for (var index = 0; index<data.length; index++){
+    console.log(data[index])
+}
