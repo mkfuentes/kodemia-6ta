5758ecc9c2d51ee372f76a002b4d5ce4a55f786b
mk fuentes <miguelangel.fm@outlook.com>
2019-12-12 16:09:14 -0600

    Salvar cambios con la funcion 11dic

--- js/main.js
+++ js/main.js
@@ -9,27 +9,58 @@
 //abstraer el objeto alumno y dado un array de alumnos retornar promedio general
 
 var alumnos = [
-    {  
-      'firstName': 'Kevin',
-      'lastName': 'Perez',
-      'age': 23,
-      'finalAverage': 8.9
-    },
-    {
-      'firstName': 'Itiel',
-      'lastName': 'Valera',
-      'age': 31,
-      'finalAverage': 6.4
-    },
-    {
-      'firstName': 'Adan',
-      'lastName': 'Mijangos',
-      'age': 30,
-      'finalAverage': 9.1
-    }
-  ]
-  const getFinalAverage = group => {
-    return group.reduce((acc, cValue) => {
-      return acc + cValue.finalAverage
-    }, 0) / group.length
+  {
+    'firstName': 'Kevin',
+    'lastName': 'Perez',
+    'age': 23,
+    'assigments': [
+      {
+        'name': 'Math',
+        'grade': 7.3 
+      },
+      {
+        'name': 'History',
+        'grade': 4 
+      }
+    ]
+  },
+  {
+    'firstName': 'Itiel',
+    'lastName': 'Valera',
+    'age': 31,
+    'assigments': [
+      {
+        'name': 'Math',
+        'grade': 9 
+      },
+      {
+        'name': 'History',
+        'grade': 6.5 
+      }
+    ]
+  },
+  {
+    'firstName': 'Adan',
+    'lastName': 'Mijangos',
+    'age': 30,
+    'assigments': [
+      {
+        'name': 'Math',
+        'grade': 6
+      },
+      {
+        'name': 'History',
+        'grade': 10 
+      }
+    ]
   }
+]
+const getFinalAverage = group => {
+  return group.reduce((acc, cValue) => {
+    return acc + cValue.assigments.reduce((acc2, cValue2) => {
+      return acc2 + cValue2.grade
+    }, 0) / cValue.assigments.length
+  }, 0) / group.length
+}
+
+console,console.log(getFinalAverage);
